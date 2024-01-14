92d4888cfe2355e8be2b7e6cb45bf6dcf92eaaea
CaelanStraus <caelan.straus@student.ehb.be>
2024-01-14 06:44:33 +0100

    0.1.3 seeder db works and roles made

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,7 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,11 +12,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        \App\Models\User::factory()->create([
+             'name' => 'admin',
+             'email' => 'admin@ehb.be',
+             'password' => 'Password!321',
+             'role' => 'admin',
+        ]);
     }
 }
