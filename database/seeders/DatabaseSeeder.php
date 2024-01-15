2003b2e1f8fc2722049b2a0d2042c82873360e6f
CaelanStraus <caelan.straus@student.ehb.be>
2024-01-15 19:42:49 +0100

    0.2.4.1 added a user seeder for easy testing

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,7 +20,16 @@ class DatabaseSeeder extends Seeder
             'password' => 'Password!321',
             'role' => 'admin',
             'dob' => '1990-01-01',
-            'bio' => 'This is the admin user.',
+            'bio' => 'This is the admin.',
+        ]);
+
+        \App\Models\User::factory()->create([
+            'name' => 'user',
+            'email' => 'user@ehb.be',
+            'password' => 'Password!321',
+            'role' => 'user',
+            'dob' => '1990-01-01',
+            'bio' => 'This is the user.',
         ]);
     }
 }
